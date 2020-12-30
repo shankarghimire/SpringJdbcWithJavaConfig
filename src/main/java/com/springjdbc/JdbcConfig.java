e18c27f32d98440a7c6a3f8862b898e621015894
Shankar Ghimire <ssghimire@gmail.com>
2020-12-30 10:41:38 -0500

    Adds CRUD operation on MySQL through Spring JDBC b

---
+++ src/main/java/com/springjdbc/JdbcConfig.java
@@ -0,0 +1,40 @@
+package com.springjdbc;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.springjdbc.dao.StudentDao;
+import com.springjdbc.dao.StudentDaoImpl;
+
+@Configuration
+public class JdbcConfig {
+	
+	@Bean(name={"dataSource"})
+	public DataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/testdb");
+		ds.setUsername("root");
+		ds.setPassword("root");
+		return ds;
+	}
+	
+	@Bean(name={"jdbcTemplate"})
+	public JdbcTemplate getJdbcTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(getDataSource());
+		return jdbcTemplate;
+	}
+	
+	@Bean(name= {"studentDao"})
+	public StudentDao getStudentDao() {
+		StudentDaoImpl studentDao = new StudentDaoImpl();
+		studentDao.setJdbcTemplate(getJdbcTemplate());
+		return studentDao;
+		
+	}
+}
